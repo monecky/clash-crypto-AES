86c7026caadef1d8d6d9cad06b6d8231f09cb35a
monecky <marijkevaniperen@gmail.com>
2025-09-29 16:35:12 +0200

    Adding KeyExpansion with TODOs

--- src/Clash/Crypto/Cipher/AES/Streaming/Algorithm.hs
+++ src/Clash/Crypto/Cipher/AES/Streaming/Algorithm.hs
@@ -21,15 +21,15 @@ Algorithm implementation of FIPS 197 using the enchance methode
 module Clash.Crypto.Cipher.AES.Streaming.Algorithm where
 
 
-import Data.Proxy (Proxy)
+
 import Clash.Crypto.Cipher.AES.Specification.Types
 import Clash.Crypto.Cipher.AES.Specification.Definitions
 import Clash.Crypto.Cipher.AES.Specification
 -- Interface liberies:
 import Clash.Prelude
 import Clash.Signal.Channel
-import Clash.Signal.DataStream
-import Clash.Signal.Extra (apWhen, regEnN)
+
+
 
 import Data.Constraint.Nat.Extra (CancelMultiple, KeepsPositiveIfMultiple)
 data CipherMode
@@ -51,12 +51,12 @@ cipher  input
       = unconcat (SNat ∷ SNat (Nb alg ))
 
     put ∷ (InType alg, WType alg) →  ((InType alg, Vec (Nr alg + 1) (RoundWType alg)), CipherMode)
-    put   (input, w)
+    put   (input1, w)
       | AESFacts{} <- knownAES @alg
-      = ((input, wInWords w), CipherStart)
+      = ((input1, wInWords w), CipherStart)
 
     get ∷ (InType alg, WType alg) → ((InType alg, Vec (Nr alg + 1) (RoundWType alg)), CipherMode) -> OutType alg
-    get _ (s0@(output, w), mode0) = output
+    get _ ((output, _), _) = output
     compute ∷ (InType alg, WType alg) → ((InType alg, Vec (Nr alg + 1) (RoundWType alg)), CipherMode) → CompMode ((InType alg, Vec (Nr alg + 1) (RoundWType alg)), CipherMode)
     compute _ (s0@(state, w), mode0) 
        | AESFacts{} ← knownAES @alg
@@ -84,17 +84,17 @@ eqInvCipher  input
   = enhance put get compute input
   where
     wInWords ∷ WType alg → Vec (Nr alg + 1) (RoundWType alg)
-    wInWords words
+    wInWords words1
       | AESFacts{} <- knownAES @alg
-      = reverse (unconcat (SNat ∷ SNat (Nb alg )) words)
+      = reverse (unconcat (SNat ∷ SNat (Nb alg )) words1)
 
     put ∷ (InType alg, WType alg) →  ((InType alg, Vec (Nr alg + 1) (RoundWType alg)), CipherMode)
-    put   (input, w)
+    put   (input1, w)
       | AESFacts{} <- knownAES @alg
-      = ((input, wInWords w), CipherStart)
+      = ((input1, wInWords w), CipherStart)
 
     get ∷ (InType alg, WType alg) → ((InType alg, Vec (Nr alg + 1) (RoundWType alg)), CipherMode) -> OutType alg
-    get _ (s0@(output, w), mode0) = output
+    get _ ((output, _), _) = output
     compute ∷ (InType alg, WType alg) → ((InType alg, Vec (Nr alg + 1) (RoundWType alg)), CipherMode) → CompMode ((InType alg, Vec (Nr alg + 1) (RoundWType alg)), CipherMode)
     compute _ (s0@(state, w), mode0) 
        | AESFacts{} ← knownAES @alg
@@ -121,17 +121,17 @@ invCipher  input
   = enhance put get compute input
   where
     wInWords ∷ WType alg → Vec (Nr alg + 1) (RoundWType alg)
-    wInWords words
+    wInWords words1
       | AESFacts{} <- knownAES @alg
-      = reverse (unconcat (SNat ∷ SNat (Nb alg )) words)
+      = reverse (unconcat (SNat ∷ SNat (Nb alg )) words1)
 
     put ∷ (InType alg, WType alg) →  ((InType alg, Vec (Nr alg + 1) (RoundWType alg)), CipherMode)
-    put   (input, w)
+    put   (input1, w)
       | AESFacts{} <- knownAES @alg
-      = ((input, wInWords w), CipherStart)
+      = ((input1, wInWords w), CipherStart)
 
     get ∷ (InType alg, WType alg) → ((InType alg, Vec (Nr alg + 1) (RoundWType alg)), CipherMode) -> OutType alg
-    get _ (s0@(output, w), mode0) = output
+    get _ ((output, _), _) = output
     compute ∷ (InType alg, WType alg) → ((InType alg, Vec (Nr alg + 1) (RoundWType alg)), CipherMode) → CompMode ((InType alg, Vec (Nr alg + 1) (RoundWType alg)), CipherMode)
     compute _ (s0@(state, w), mode0) 
        | AESFacts{} ← knownAES @alg
@@ -147,5 +147,111 @@ invCipher  input
       CipherLast 2                       → ((invShiftRows state,w),                    CipherLast 1)
       CipherLast 1                       → ((invSubBytes state,w),                   CipherLast 0)
       CipherLast 0                       → ((invAddRoundKey state (last w),w),        CipherFin)
+data KeyMode
+  = KeyStart | KeyNew (Index 2) Integer | KeyLast (Index 4) Integer | KeyFin | KeyEnd
+  deriving (Generic, NFDataX, Show, Eq)
+class AESKeyExpansion (alg ∷ AES) where
+    keyExpansion ∷ (KnownAES alg, HiddenClockResetEnable dom) ⇒
+      Channel dom (KeyType alg) →
+      -- ^ input stream ^ key stream
+      Channel dom (WType alg)
+      -- ^ response channel
+instance AESKeyExpansion AES128 where
+  keyExpansion ∷
+      (KnownAES AES128, HiddenClockResetEnable dom) ⇒
+      Channel dom (KeyType AES128) →
+      -- ^ input stream ^ key stream
+      Channel dom (WType AES128)
+      -- ^ response channel
+  keyExpansion = enhance put get compute
+      where
+        put ∷ ∀ alg. (KnownAES alg, alg ~ AES128) ⇒ KeyType alg →  ((KeyType alg, WordType alg, WType alg), KeyMode)
+        put key -- state, result head00000 @key(1,2,3,4,5,...)  last (shiftInAtN will make it too front) and rotateRight key
+          | AESFacts _ ← knownAES @alg
+          = ((key, last key, repeat  @(((Nr alg + 1) * 4) -  Nk alg) (repeat  @(WordSize alg) (v2bv (repeat @(ByteSize alg) low))) ++ key), KeyStart)
+
+        get ∷ ∀ alg. (KnownAES alg, alg ~ AES128) ⇒ KeyType alg → ((KeyType alg, WordType alg, WType alg), KeyMode) -> WType alg
+        get _ ((_, _, w), _) = w
+        compute ∷ ∀ alg. (KnownAES alg, alg ~ AES128) ⇒  KeyType alg → ((KeyType alg, WordType alg, WType alg), KeyMode) → CompMode ((KeyType alg, WordType alg, WType alg), KeyMode)
+        compute _ (s0@(state, lastState, w), mode0) 
+          | AESFacts alg ← knownAES @alg
+          = (, mode0 /= KeyEnd) $ case mode0 of
+          KeyEnd                             → (s0,                                                                             mode0)
+          KeyFin                             → (s0,                                                                             KeyEnd)
+          KeyStart                           → (s0,                                                                             KeyLast 3 (natToInteger @(Nr alg)))
+          KeyLast 3 i                        → ((state, rotWord lastState ,w),                                                  KeyLast 2 i)
+          KeyLast 2 i                        → ((state, subWord lastState ,w),                                                  KeyLast 1 i)
+          KeyLast 1 i                        → ((state, xorWord lastState (_Rcon alg !! ((natToInteger @(Nr alg)) - i)),w),     KeyLast 0 (i-1))
+          KeyLast 0 i                        → ((postscanl xorWord lastState state, lastState , w),                             KeyNew 0 i)
+          KeyNew 0 i                         → ((state, last state, shiftNewPart w state),                                      KeyLast 3 (i - 1))
+          KeyNew 0 (-1)                      → (s0,                                                                             KeyFin)
+        shiftNewPart ∷ ∀ alg. (KnownAES alg, alg ~ AES128) ⇒ WType alg → KeyType alg -> WType alg
+        shiftNewPart w state = fst (shiftInAtN w state)
+instance AESKeyExpansion AES192 where
+  keyExpansion ∷
+      (KnownAES AES192, HiddenClockResetEnable dom) ⇒
+      Channel dom (KeyType AES192) →
+      -- ^ input stream ^ key stream
+      Channel dom (WType AES192)
+      -- ^ response channel
+  keyExpansion = enhance put get compute
+      where
+        put ∷ ∀ alg. (KnownAES alg, alg ~ AES192) ⇒ KeyType alg →  ((KeyType alg, WordType alg, WType alg), KeyMode)
+        put key -- state, result head00000 @key(1,2,3,4,5,...)  last (shiftInAtN will make it too front) and rotateRight key
+          | AESFacts _ ← knownAES @alg
+          -- TODO the solution shift too much.
+          = ((key, last key, repeat @(((Nr alg + 1) * 4) -  Nk alg) (repeat  @(WordSize alg) (v2bv (repeat @(ByteSize alg) low))) ++ key), KeyStart)
+
+        get ∷ ∀ alg. (KnownAES alg, alg ~ AES192) ⇒ KeyType alg → ((KeyType alg, WordType alg, WType alg), KeyMode) -> WType alg
+        get _ ((_, _, w), _) = w
+        compute ∷ ∀ alg. (KnownAES alg, alg ~ AES192) ⇒  KeyType alg → ((KeyType alg, WordType alg, WType alg), KeyMode) → CompMode ((KeyType alg, WordType alg, WType alg), KeyMode)
+        compute _ (s0@(state, lastState, w), mode0) 
+          | AESFacts alg ← knownAES @alg
+          = (, mode0 /= KeyEnd) $ case mode0 of
+          KeyEnd                             → (s0,                                                                             mode0)
+          KeyFin                             → (s0,                                                                             KeyEnd)
+          KeyStart                           → (s0,                                                                             KeyLast 3 (natToInteger @(Nr alg)))
+          KeyLast 3 i                        → ((state, rotWord lastState ,w),                                                  KeyLast 2 i)
+          KeyLast 2 i                        → ((state, subWord lastState ,w),                                                  KeyLast 1 i)
+          KeyLast 1 i                        → ((state, xorWord lastState (_Rcon alg !! ((natToInteger @(Nr alg)) - i)),w),     KeyLast 0 (i-1))
+          KeyLast 0 i                        → ((postscanl xorWord lastState state, lastState , w),                             KeyNew 0 i)
+          KeyNew 0 i                         → ((state, last state, shiftNewPart w state),                                      KeyLast 3 (i - 1))
+          KeyNew 0 (-1)                      → (s0,                                                                             KeyFin)
+        shiftNewPart ∷ ∀ alg. (KnownAES alg, alg ~ AES192) ⇒ WType alg → KeyType alg -> WType alg
+        shiftNewPart w state = fst (shiftInAtN w state)
+
+instance AESKeyExpansion AES256 where
+  keyExpansion ∷
+      (KnownAES AES256, HiddenClockResetEnable dom) ⇒
+      Channel dom (KeyType AES256) →
+      -- ^ input stream ^ key stream
+      Channel dom (WType AES256)
+      -- ^ response channel
+  keyExpansion = enhance put get compute
+      where
+        put ∷ ∀ alg. (KnownAES alg, alg ~ AES256) ⇒ KeyType alg →  ((KeyType alg, WordType alg, WType alg), KeyMode)
+        put key -- state, result head00000 @key(1,2,3,4,5,...)  last (shiftInAtN will make it too front) and rotateRight key
+          | AESFacts _ ← knownAES @alg
+          = ((key, last key, repeat  @(((Nr alg + 1) * 4) -  Nk alg) (repeat  @(WordSize alg) (v2bv (repeat @(ByteSize alg) low))) ++ key), KeyStart)
+
+        get ∷ ∀ alg. (KnownAES alg, alg ~ AES256) ⇒ KeyType alg → ((KeyType alg, WordType alg, WType alg), KeyMode) -> WType alg
+        get _ ((_, _, w), _) = w
+        compute ∷ ∀ alg. (KnownAES alg, alg ~ AES256) ⇒  KeyType alg → ((KeyType alg, WordType alg, WType alg), KeyMode) → CompMode ((KeyType alg, WordType alg, WType alg), KeyMode)
+        compute _ (s0@(state, lastState, w), mode0) 
+          | AESFacts alg ← knownAES @alg
+          = (, mode0 /= KeyEnd) $ case mode0 of
+          KeyEnd                             → (s0,                                                                             mode0)
+          KeyFin                             → (s0,                                                                             KeyEnd)
+          KeyStart                           → (s0,                                                                             KeyLast 3 (natToInteger @(Nr alg)))
+          KeyLast 3 i                        → ((state, rotWord lastState ,w),                                                  KeyLast 2 i)
+          KeyLast 2 i                        → ((state, subWord lastState ,w),                                                  KeyLast 1 i)
+          KeyLast 1 i                        → ((state, xorWord lastState (_Rcon alg !! ((natToInteger @(Nr alg)) - i)),w),     KeyLast 0 (i-1))
+          -- TODO: There needs to be a subWord in between
+          KeyLast 0 i                        → ((postscanl xorWord lastState state, lastState , w),                             KeyNew 0 i)
+          KeyNew 0 i                         → ((state, last state, shiftNewPart w state),                                      KeyLast 3 (i - 1))
+          KeyNew 0 (-1)                      → (s0,                                                                             KeyFin)
+        shiftNewPart ∷ ∀ alg. (KnownAES alg, alg ~ AES256) ⇒ WType alg → KeyType alg -> WType alg
+        shiftNewPart w state = fst (shiftInAtN w state)
+
 
 
