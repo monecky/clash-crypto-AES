dc02864c923debb7ce095a6b1f92c357408fa2fc
monecky <marijkevaniperen@gmail.com>
2025-10-09 15:55:57 +0200

    dummy implementation of algorithm 5

--- src/Clash/Crypto/PQC/SLH_DSA/Specification/Definitions/WOTSplus.hs
+++ src/Clash/Crypto/PQC/SLH_DSA/Specification/Definitions/WOTSplus.hs
@@ -18,11 +18,12 @@ import Clash.Crypto.PQC.SLH_DSA.General.General
 import Clash.Crypto.PQC.SLH_DSA.Specification.Types
 
 import Clash.Crypto.PQC.SLH_DSA.Specification.Definitions
-
+-- Algorithm 5
 chain ∷ ∀ i s ℓ (alg ∷ SLH_DSA). (KnownNat ℓ, KnownNat i, KnownNat s, 1 ≤ i, s + 1 ≤ (ℓ - i) + 1, SLH_DSA_hash alg) ⇒ BitVector ℓ → PKSeedType alg → ADRSType alg → BitVector ℓ
-chain x pkSeed adrs = v2bv $ foldl (function) tmp (iterateI @s (+1) (natToNum @i @(Index ℓ))) (select @(ℓ - i) (SNat ∷ SNat i) (SNat ∷ SNat 1) (SNat ∷ SNat s) tmp)
+chain x pkSeed adrs = v2bv $ foldl (function) tmp (iterateI @s (+1) (natToNum @i @(Index ℓ)))
     where
         function ∷ Vec ℓ Bit → Index n → Vec ℓ Bit
         function x1 j = x1 -- TODO _F pkSeed (setHashAddress adrs j) x
         tmp ∷ Vec ℓ Bit
         tmp = bv2v x
+-- Algorithm 6
