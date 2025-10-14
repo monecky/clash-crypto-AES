64bee10c135f17c5a3255cafbcb11140d524b503
monecky <marijkevaniperen@gmail.com>
2025-10-14 10:29:20 +0200

    fix typo header

--- src/Clash/Crypto/Hash/MGF1/Specification.hs
+++ src/Clash/Crypto/Hash/MGF1/Specification.hs
@@ -1,5 +1,5 @@
 {-|
-Module      : Clash.Crypto.Cipher.AES
+Module      : Clash.Crypto.Hash.MGF1.Specification
 Copyright   : Copyright © 2025 QBayLogic B.V.
 Maintainer  : QBayLogic B.V.
 Stability   : experimental
