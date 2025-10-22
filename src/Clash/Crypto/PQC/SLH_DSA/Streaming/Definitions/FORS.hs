3c3587b06e7ffe0d284fe83c5ff5aed297754e02
monecky <marijkevaniperen@gmail.com>
2025-10-22 15:39:30 +0200

    Adding FORS  file for chapter 8 on FIPS205

---
+++ src/Clash/Crypto/PQC/SLH_DSA/Streaming/Definitions/FORS.hs
@@ -0,0 +1,11 @@
+{-|
+Module      : Clash.Crypto.PQC.SLH_DSA.Streaming.Definitions.FORS
+Copyright   : Copyright © 2025 QBayLogic B.V.
+Maintainer  : QBayLogic B.V.
+Stability   : experimental
+Portability : POSIX
+
+Basic FORS definitions covering the fundamentals of FIPS 205.
+-}
+
+module Clash.Crypto.PQC.SLH_DSA.Streaming.Definitions.FORS where
