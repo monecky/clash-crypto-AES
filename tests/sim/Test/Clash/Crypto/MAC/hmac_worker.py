2cb32a2f970a7ea3d3204e240ec0dc958939f9df
monecky <marijkevaniperen@gmail.com>
2025-11-17 15:51:52 +0100

    clean up usage comment.

--- tests/sim/Test/Clash/Crypto/MAC/hmac_worker.py
+++ tests/sim/Test/Clash/Crypto/MAC/hmac_worker.py
@@ -5,7 +5,7 @@ from slhdsa.lowlevel.parameters import hmac_digest
 
 def main():
     if len(sys.argv) != 4:
-        print("Usage: hmac_worker.py <hexKey> <hexMsg> <digestName> [blocksize]", file=sys.stderr)
+        print("Usage: hmac_worker.py <hexKey> <hexMsg> <digestName>", file=sys.stderr)
         sys.exit(1)
 
     hex_key, hex_msg, digest_name = sys.argv[1:4]
