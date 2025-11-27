6599d2da9d1e68d7226d5db8859e2fb4f76e4e84
monecky <marijkevaniperen@gmail.com>
2025-11-27 13:51:33 +0100

    Adding more description

--- tests/sim/Test/Clash/Crypto/Cipher/AES/TestConstantClockCycle/TestConstantClockCycle.py
+++ tests/sim/Test/Clash/Crypto/Cipher/AES/TestConstantClockCycle/TestConstantClockCycle.py
@@ -45,7 +45,7 @@ def check_equal(arr, name):
         print(f"All durations of {name} are equal")
     else:
         print(f"The durations of {name} differ")
-
+print("")
 check_equal(AES128, "AES128")
 check_equal(AES192, "AES192")
 check_equal(AES256, "AES256")
