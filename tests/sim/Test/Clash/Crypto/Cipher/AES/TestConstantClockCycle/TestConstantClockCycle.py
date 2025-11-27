04bdea205bf92880c472f8e5cf7da95f14a914b4
monecky <marijkevaniperen@gmail.com>
2025-11-27 13:54:15 +0100

    Clean up code of the AES python test.

--- tests/sim/Test/Clash/Crypto/Cipher/AES/TestConstantClockCycle/TestConstantClockCycle.py
+++ tests/sim/Test/Clash/Crypto/Cipher/AES/TestConstantClockCycle/TestConstantClockCycle.py
@@ -15,15 +15,7 @@ else:
 # How many tests per AES algorithm
 number_tests = 100
 df = pd.read_csv("digital.csv")
-df["start_time"] = pd.to_datetime(df["start_time"])
-main_async = df[df["name"] == "Async Serial"].copy()
-sub_async = df[df["name"] == "Async Serial [1]"].copy()
-
-main_async = main_async.sort_values("start_time")
-sub_async = sub_async.sort_values("start_time")
 time_differences = []
-
-
 for index, row in df.iterrows():
     for next_index, next_row in df.iterrows():
         if index + 1 == next_index:
@@ -33,12 +25,9 @@ for index, row in df.iterrows():
                     time_differences.append(delta_us)
                     with open(file_path, "a") as f:
                         f.write(f"{delta_us:.3f} µs\n")
-
 AES128 = time_differences[0:number_tests]
 AES192 = time_differences[number_tests:2*number_tests]
 AES256 = time_differences[2*number_tests:3*number_tests]
-
-# Check if all values are equal
 def check_equal(arr, name):
     #  First case covers the standard cycle, the other one covers rounding errors regards floating points.
     if len(set(arr)) == 1 or (len(set(arr)) == 2 and set(arr)[0] in {set(arr)[1] -1,set(arr)[1]+1}) :
