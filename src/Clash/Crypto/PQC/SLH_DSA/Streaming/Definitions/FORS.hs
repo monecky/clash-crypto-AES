3362eebea5cec200be19ebae6ebbf6733b9fa05c
monecky <marijkevaniperen@gmail.com>
2025-12-04 17:37:42 +0100

    fors_nodeOpt frame work

--- src/Clash/Crypto/PQC/SLH_DSA/Streaming/Definitions/FORS.hs
+++ src/Clash/Crypto/PQC/SLH_DSA/Streaming/Definitions/FORS.hs
@@ -73,7 +73,10 @@ fors_node input⁰ =  mux  (fmap (== 0x00) z) ifthen ifelse
         ifthen ∷ Channel dom (NodeType alg)
         ifthen 
             | SLH_DSAParametersFacts {} ← knownSLH_DSAParameters @alg
-            = _FStream (liftA2 (\(s,t,v) w → (t, setTreeIndex (setTreeHeight v (0x0 ∷ BitVector (ChainAddressTreeHeightSize alg * ByteSize))) w, s)) input i)
+            = _FStream (liftA3 (\(s,t,v) w sk → (t, setTreeIndex (setTreeHeight v (0x0 ∷ BitVector (ChainAddressTreeHeightSize alg * ByteSize))) w, sk)) input i skSeed)
+            where 
+                skSeed ∷ Channel dom (SKPrfType alg)
+                skSeed = fors_skGen (liftA2 (\(s,t,v) w → (s,t,v,w)) input i)
         ifelse ∷ Channel dom (NodeType alg)
         ifelse
             | SLH_DSAParametersFacts alg ← knownSLH_DSAParameters @alg
@@ -100,8 +103,20 @@ type ForsNodeState alg  =
   Index (K alg * (2 ^(A alg))), -- i to find
   Index (A alg) -- z to find
  )
-data ForsNodeMode (alg ∷ SLH_DSA) 
-  = KeyStart | KeyProsXOR (Index 3) (Index (Nr alg + 1)) | KeyProsLastW (Index 4) (Index (Nr alg + 1))  | KeyFin | KeyEnd
+type ForsNodeMealState alg = (IdxType alg{-Current i-},
+                              IdxType alg{-Current z-},
+                              Vec (A alg) (NodeType alg) {-Buffer-},
+                              Bool{-Result is ready-},
+                              IdxType alg{-To calculate i-},
+                              IdxType alg{-To calculate z-}, 
+                              Bool{-Computation going on-},
+                              NodeType alg{-Previous output-})
+type ForsNodeMealInput alg = ((Maybe (IdxType alg{-i-}), Bool),
+                              (Maybe (IdxType alg{-z-}), Bool), 
+                              (Maybe (NodeType alg{-h-}), Bool), 
+                              (Maybe (NodeType alg{-f-}), Bool))
+type ForsNodeMealOutput alg = ((IdxType alg{-i-}, IdxType alg{-z-},NodeType alg{-lNode-}, NodeType alg{-rNode-}, NodeType alg{-Result-}, Bool), ProviderAction)
+
 -- Algorithm 15
 fors_nodeOpt ∷ ∀ (alg ∷ SLH_DSA)  dom . (KnownDomain dom, HiddenClockResetEnable dom,  KnownSLH_DSAParameters alg, SLH_DSA_hashStreamFact alg) 
     ⇒ Channel dom (SKSeedType alg, PKSeedType alg, ADRSType alg
@@ -111,7 +126,7 @@ fors_nodeOpt ∷ ∀ (alg ∷ SLH_DSA)  dom . (KnownDomain dom, HiddenClockReset
      → Channel dom (NodeType alg)
 fors_nodeOpt input⁰ 
     | SLH_DSAParametersFacts {} ← knownSLH_DSAParameters @alg
-    = go <$> cResult <*> cReady
+    = go cResult cReady
     where
         i ∷ Channel dom (IdxType alg)
         i = fmap (\(s,p,a,x,y) → x) input⁰
@@ -123,36 +138,75 @@ fors_nodeOpt input⁰
         pkSeed = fmap (\(s,p,a,x,y) → p) input⁰
         adrs ∷ Channel dom (ADRSType alg)
         adrs = fmap (\(s,p,a,x,y) → a) input⁰
-        controller ∷ Channel dom (NodeType alg{-i-}, NodeType alg{-z-},NodeType alg{-lNode-}, NodeType alg{-rNode-}, NodeType alg{-Result-}, Bool) -- NodeType alg{-H-}, NodeType alg{-F-})
-        controller = mealy (~~>) ((,,,) <$> channel2Signal i <*> channel2Signal z <*> channel2Signal hInstance <*> channel2Signal fInstance)
+        controller ∷ Channel dom (IdxType alg{-i-}, IdxType alg{-z-},NodeType alg{-lNode-}, NodeType alg{-rNode-}, NodeType alg{-Result-}, Bool) -- NodeType alg{-H-}, NodeType alg{-F-})
+        controller 
+            | SLH_DSAParametersFacts {} ← knownSLH_DSAParameters @alg
+            = channel (mealy (~~>) 
+            (   -- Initial state
+                0x0∷ IdxType alg{-Current i-},
+                0x0 ∷ IdxType alg{-Current z-},
+                unconcatI (unconcatBitVector# 0x0) ∷ Vec (A alg) (NodeType alg) {-Buffer-},
+                False ∷ Bool{-Result is ready-},
+                0x0 ∷ IdxType alg{-To calculate i-},
+                0x0 ∷ IdxType alg{-To calculate z-}, 
+                False ∷ Bool{-Computation going on-},
+                unconcatBitVector# 0x0 ∷ NodeType alg{-Previous output-}
+            )
+            ((,,,) <$> channel2Signal i <*> channel2Signal z <*> channel2Signal hInstance <*> channel2Signal fInstance))
             where
-                (~~>) ∷ 
+                (~~>) ∷ (ForsNodeMealState alg
+                    → ForsNodeMealInput alg
+                    → (ForsNodeMealState alg, ForsNodeMealOutput alg))
+                (~~>) state@(_,_,_,_,_,_,False,x) input@((Just curI, True), (Just depZ, True),_,_)  
+                    | SLH_DSAParametersFacts {} ← knownSLH_DSAParameters @alg
+                    = (state, ((0x0 ∷ IdxType alg{-Current i-},
+                        0x0 ∷ IdxType alg{-Current z-},
+                        unconcatBitVector# 0x0 ∷ NodeType alg {-lNode-},
+                        unconcatBitVector# 0x0 ∷ NodeType alg {-lNode-},
+                        x ∷ NodeType alg {-Result-}, False),Keep))
+                (~~>) state@(_,_,_,_,_,_,_,x) input@(_, _, _, _)  
+                    | SLH_DSAParametersFacts {} ← knownSLH_DSAParameters @alg
+                    = (state, ((0x0 ∷ IdxType alg{-Current i-},
+                        0x0 ∷ IdxType alg{-Current z-},
+                        unconcatBitVector# 0x0 ∷ NodeType alg {-lNode-},
+                        unconcatBitVector# 0x0 ∷ NodeType alg {-lNode-},
+                        x ∷ NodeType alg {-Result-}, False),Keep))
+                -- (~~>) state@(_,_,_,_,_,_,_,_) input@(_, _, _, _)  = (state, (Nothing, Clear))
         cI = fstOf6C controller
         cZ = sndOf6C controller
         cLNode= thdOf6C controller
         cRNode = frtOf6C controller
         cResult = fthOf6C controller
         cReady = sthOf6C controller
+        channel2Signal ∷ Channel dom s → Signal dom (Maybe s, Bool)
         channel2Signal inputC = (liftA2 (,) (content inputC) (hasUpdates inputC))
         go ∷ Channel dom (NodeType alg{-Result-}) → Channel dom (Bool) → Channel dom (NodeType alg{-When ready-})
-        go result¹ ready¹ = channel $ mealy (~~>) (0x0) (channel2Signal inputC)
+        go result¹ ready¹ 
+            | SLH_DSAParametersFacts {} ← knownSLH_DSAParameters @alg
+            = channel $ mealy (~~>) (unconcatBitVector# 0x0) (channel2Signal inputC)
             where
                 inputC = (zipC result¹ ready¹)
-                (~~>) (_) ((Just x, True), True) = (x, Release)
-                (~~>) (x) ((_, _), _) = (x, Keep)    
+                (~~>) ∷ (NodeType alg)
+                        → (Maybe ((NodeType alg), Bool), Bool)
+                        → ((NodeType alg), (NodeType alg, ProviderAction))
+                (~~>) state@(_) input@((Just (x, True)), True) = (x, (x, Release))
+                (~~>) state@(x) input@(_, _) = (state, (x, Keep))    
         hInstance ∷ Channel dom (NodeType alg)
         hInstance
             | SLH_DSAParametersFacts alg ← knownSLH_DSAParameters @alg
-            = _HStream ((\p a l r →  (p,a, l ‖ r)) pkSeed  adrs¹ cLNode cRNode)
+            = _HStream ((\p a l r →  (p,a, l ‖ r)) <$> pkSeed <*> adrs¹ <*> cLNode <*> cRNode)
             where
                 adrs¹ ∷ Channel dom (ADRSType alg)
                 adrs¹ 
                     | SLH_DSAParametersFacts {} ← knownSLH_DSAParameters @alg
                     = liftA3 (\a v w → setTreeIndex (setTreeHeight a (v ∷ BitVector (ChainAddressTreeHeightSize alg * ByteSize))) w) adrs cI cZ
         fInstance ∷ Channel dom (NodeType alg)
-        fInstance input¹ adrs¹ lnode¹ rnode¹
+        fInstance
             | SLH_DSAParametersFacts alg ← knownSLH_DSAParameters @alg
-            = _FStream (liftA2 (\a w → (t, setTreeIndex (setTreeHeight v (0x0 ∷ BitVector (ChainAddressTreeHeightSize alg * ByteSize))) w, s)) adrs cI)
+            = _FStream @alg ((\a w sk p → (p, setTreeIndex (setTreeHeight a (0x0 ∷ BitVector (ChainAddressTreeHeightSize alg * ByteSize))) w, sk)) <$> adrs <*> cI <*> skPrf <*> pkSeed)
+            where 
+                skPrf ∷ Channel dom (SKPrfType alg)
+                skPrf = fors_skGen (liftA2 (\(s,t,v) w → (s,t,v,w)) input cI)
 
 -- Algorithm 16
 fors_sign ∷ ∀ (alg ∷ SLH_DSA)  dom . (KnownDomain dom, HiddenClockResetEnable dom,  KnownSLH_DSAParameters alg, SLH_DSA_hashStreamFact alg) 
