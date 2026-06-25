clash-lang/clash-crypto

Passing tests for hitlt

tests/hitl/app/Main.hs
@@ -284,7 +284,7 @@ runHitltAES ∷
 runHitltAES sem dev settings input key | AESFacts alg ← knownAES @alg =
  let
   bs = (append input key)
-  eq = encryptoECB alg input key
+  eq = encryptoECB alg key input
  in runHitlt @(WordSize alg * Nb alg) sem dev settings bs eq
 --  in runHitlt @((Nr alg + 1) * 4) sem dev settings bs eq
 runHitltInverseModulo ∷

monecky/clash-crypto-AES

tests/hitl/app/Main.hs
@@ -427,7 +427,7 @@ runHitltAES ∷
 runHitltAES alg sem dev settings input key | AESFacts aes ← knownAES @alg =
  let
   bs = (append input key)
-  eq = encryptoECB aes input key
+  eq = encryptoECB aes key input
  in runHitlt (type (WordSize alg * Nb alg)) sem dev settings bs eq
 --  in runHitlt (type ((Nr alg + 1) * 4)) sem dev settings bs eq
 